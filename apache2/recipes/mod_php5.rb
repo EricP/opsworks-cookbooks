3b1a06204c05a1ed484eaae601a5e5001e1f2157
EricP <eric.pecoraro@shepardapps.com>
2016-03-30 12:20:34 -0400

    Update mod_php5.rb

--- apache2/recipes/mod_php5.rb
+++ apache2/recipes/mod_php5.rb
@@ -34,6 +34,8 @@ bash 'install php56' do
       echo "INSTALLING PHP 5.6 (INCLUDES APACHE 2.4)"
       sudo yum -y install php56
       sudo yum -y install php56-opcache php56-mysqlnd php56-bcmath php56-devel php56-gd php56-mbstring php56-mcrypt php56-pdo php56-soap php56-xmlrpc php56-pecl-memcache
+      # install httpd-tools needed?
+      # yum -y install httpd-tools
       # Fix apache user to allow httpd commands (outlined in .dev/commands/resolve.perms.sh)
       sudo useradd -g apache -d /var/www apache
       #
